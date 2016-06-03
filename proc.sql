854dcbc96f77f7845b38e677893478e81d67e2c2
pcgeller <pcgeller@gmail.com>
2016-06-03 17:35:33 -0400

    fixed error in loading code. split out load of big files

---
+++ proc.sql
@@ -0,0 +1,11 @@
+CREATE TABLE proc (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  tstamp integer(0),
+  userdomain varchar(20),
+  computer varchar(20),
+  processname varchar(20),
+  startend varchar(20));
+
+load data local infile '/home/pcgeller/weirdo/data/proc.txt'
+  into table proc
+  fields terminated by ',' lines terminated by '\n'
+  (tstamp, userdomain,  computer, processname, startend);
