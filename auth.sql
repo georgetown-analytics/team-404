ad8eb0e4756f35ddfe0d3565b0eee649edab3739
pcgeller <pcgeller@gmail.com>
2016-07-03 17:50:08 -0400

    rewrote database init into postgres

--- auth.sql
+++ auth.sql
@@ -1,22 +1,18 @@
-#setup auth
-CREATE TABLE auth (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  tstamp INTEGER(0),
-  srcuserdomain VARCHAR(20),
-  dstuserdomain VARCHAR(20),
-  srccomputer VARCHAR(20),
-  dstcomputer VARCHAR(20),
-  authtype VARCHAR (20),
-  logontype VARCHAR (20),
-  authorient VARCHAR (20),
-  passfail VARCHAR (15),
-  srcusr VARCHAR(10),
-  srcdomain VARCHAR(15)
-  dstusr VARCHAR(10),
-  dstdomain VARCHAR(15),
-  redteam VARCHAR(5);
+--setup auth
+CREATE TABLE auth (id SERIAL,
+  tstamp integer,
+  srcuserdomain VARCHAR,
+  dstuserdomain VARCHAR,
+  srccomputer VARCHAR,
+  dstcomputer VARCHAR,
+  authtype VARCHAR,
+  logontype VARCHAR,
+  authorient VARCHAR,
+  passfail VARCHAR,
+  srcusr VARCHAR,
+  srcdomain VARCHAR,
+  dstusr VARCHAR,
+  dstdomain VARCHAR,
+  redteam VARCHAR;
 
-LOAD DATA LOCAL INFILE '/media/pcgeller/SharedDrive/auth.txt'
-  INTO TABLE auth
-  FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
-  (tstamp, srcuserdomain, dstuserdomain, srccomputer, dstcomputer, authtype, logontype, authorient, 	passfail);
-
+\COPY auth(tstamp,srcuserdomain,dstuserdomain,srccomputer,dstcomputer,authtype,logontype,authorient,passfail) FROM '/home/pcgeller/weirdo/data/auth.txt' DELIMITER ',' CSV;
