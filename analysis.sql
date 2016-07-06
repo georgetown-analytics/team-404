1923effe1aee20a089674f64bbc073ab1330c21b
pcgeller <pcgeller@gmail.com>
2016-07-05 20:00:17 -0400

    started munge

---
+++ analysis.sql
@@ -0,0 +1,31 @@
+/*# Create table of whose logged on
+
+
+#select * from auth
+#	where srccomputer = 'C1003';
+#select * from proc
+#	where computer = 'C1003';
+#select * from flows
+#	where srccomputer = 'C1003';
+#select * from dns
+#	where srccomputer = 'C1003';
+
+create table aglobal
+	#select dstcomputer, count(*) as dstfreq from auth group by dstcomputer
+	select authorient, count(*) as orientfreq from auth group by authorient
+	select logontype, count(*) as typefreq from auth group by longontype
+	select passfail, count(*) as passfailfreq from auth group by passfail;
+
+create table atest
+	select dstcomputer, count(*) as dstfreq from authtest group by dstcomputer
+	select authorient, count(*) as orientfreq from authtest group by authorient
+	select logontype, count(*) as typefreq from authtest group by longontype
+	select passfail, count(*) as passfail from authtest group by passfail;
+
+create table pglobal
+	select processname, count(*) as freq from proc group by processname;
+
+create table ptest
+	select processname, count(*) as freq from proctest group by processname;
+*/
+
