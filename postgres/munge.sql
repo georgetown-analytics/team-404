8caa52354e1c00815a9389fc45004a3cc4028d22
pcgeller <pcgeller@gmail.com>
2016-07-16 10:09:00 -0400

    clean up extra characters in some fields, started maintenance script

--- postgres/munge.sql
+++ postgres/munge.sql
@@ -10,13 +10,25 @@ UPDATE redteam
 set usr = split_part(userdomain, '@', 1)
 set domain = split_part(userdomain, '@', 2);
 */
-
+/*
 UPDATE proc
 set comp = split_part(compdomain, '$@', 1),
 domain = split_part(compdomain, '$@', 2);
-
+*/
+/*
 UPDATE auth
 set srcusr = split_part(srcuserdomain, '@', 1),
 srcdomain = split_part(srcuserdomain, '@', 2),
 dstusr = split_part(dstuserdomain, '@', 1),
 dstdomain = split_part(dstuserdomain, '@', 2);
+*/
+
+UPDATE proc
+SET comp = split_part(comp,'@',1)
+SET domain = split_part(comp,'@',2);
+
+UPDATE auth
+SET srcusr = split_part(srcusr,'$',1)
+SET dstusr = split_part(dstusr,'$',1);
+
+
