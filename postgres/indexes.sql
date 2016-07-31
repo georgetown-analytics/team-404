4a26ae3b2631a563ece78454cbd5d51d3a966f68
pcgeller <pcgeller@gmail.com>
2016-07-31 09:12:09 -0400

    laptop push

--- postgres/indexes.sql
+++ postgres/indexes.sql
@@ -15,12 +15,13 @@ create index procprocessname on proc (processname);
 create index procusr on proc (usr);
 create index procdomain on proc (domain);
 */
-
+/*
 create index authsrccomp on auth (srccomputer);
 create index authdstcomp on auth (dstcomputer);
 create index authsrcuser on auth (srcuser);
 create index authdstuser on auth (dstuser);
 create index authsrcdomain on auth (srcdomain);
 create index authdstdomain on auth (dstdomain);
+*/
 
-
+CREATE INDEX tstamp ON auth(srccomputer);
